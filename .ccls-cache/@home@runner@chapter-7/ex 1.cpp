58688804e67170efce26059c0790fddd22d3e022
avros16 <114757828+avros16@users.noreply.github.com>
2024-07-25 14:17:50 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@chapter-7/ex 1.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// using namespace std;
+
+
+
+// struct Student {
+// int num;
+// char name[20];
+// char gender;
+// double score;
+
+// };
+
+
+// int main() {
+
+// Student student1 = {10001, "Shaobai", 'M', 97.2};
+//  // Student students2 = {10010, "Kathy", 'F', 85};
+
+//   cout << student1.num << "\t" << student1.name << "/t" << student1.gender << "\t" << student1.score << endl;
+
+//   return 0;
+
+
+
+
+// }
---
+++ .ccls-cache/@home@runner@chapter-7/ex 2.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+// using namespace std;
+
+
+
+// struct Student {
+// int num;
+// char name[20];
+// char gender;
+// double score;
+
+// };
+
+
+// int main() {
+
+// Student student1;
+
+//   student1.num =10001;
+//   strcpy_s(student1.name, "Shaobai");
+//   student1.gender ='M';
+//   student1.score =97.2;
+
+//   cout << student1.num << "\t" << student1.name << "/t" << student1.gender << "\t" << student1.score << endl;
+
+//   return 0;
+
+
+
+
+// }
